fffa8601d014e062c0b506fed26c17116aac2902
Sturla Magnus Grøndal <SansMorel@users.noreply.github.com>
2022-06-13 11:16:04 +0200

    Document SpillComp

--- R/SpillComp.R
+++ R/SpillComp.R
@@ -1,3 +1,15 @@
+#' Estimate spillover matrix and correct spillover
+#' @param data A numerical matrix.
+#' @param n Number of sampled events to be used for spillover estimation. n=0 uses all events.
+#' @param cols Columns to be used for spillover estimation and spillover correction.
+#' @param output Output FCS filename. Set output = NULL  to not write FCS file.
+#' @param threshold Spillover max boundary. See https://onlinelibrary.wiley.com/doi/10.1002/cyto.a.24298.
+#' @param flexrep Passed to flexmix. Number of times flexmix is run. See flexmix nrep.
+#' @param neighbor Abundance spillover channels. Can be 1 or 2.
+#' @param seed seed for reproducibility.
+#' @param n_threads Number of threads used for calculation of cutoffs.
+#' @references
+#'  Miao, Q, Wang, F, Dou, J, et al. Ab initio spillover compensation in mass cytometry data. Cytometry. 2021; 99: 899– 909. https://doi.org/10.1002/cyto.a.24298
 #' @export
 SpillComp <- function(data, n = 1e4, cols = NULL, output = NULL, threshold = 0.1, flexrep = 10, neighbor = 1, seed = 42, n_threads = 8) {
 
